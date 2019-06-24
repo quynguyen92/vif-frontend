d9f38553042c6090019a9c05cc188e88435f11f2
quynguyen92 <nguyenquy101192@gmail.com>
2019-06-25 01:00:14 +0700

    update lib

--- wp-content/themes/vif/index.php
+++ wp-content/themes/vif/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <?php get_header(); ?>
-<body class="">
+<body>
 <div id="page" class="site">
     <header id="masthead" class="site-header ">
         <div class="elementor elementor-23">
@@ -9,48 +9,23 @@
                 <div class="elementor-section-wrap">
                     <section>
                         <div class="elementor-container">
-                            <div class="elementor-row">
+                            <div class="row">
                                 <div class="">
                                     <div class="">
                                         <div class="elementor-widget-wrap">
                                             <div class="">
-                                                <div class="elementor-widget-container">
-                                                    <div class="elementor-social-icons-wrapper">
-                                                        <a class="elementor-icon elementor-social-icon elementor-social-icon-facebook" href="https://www.facebook.com/zemezlab/" target="_blank"><span class="elementor-screen-only">Facebook</span><i class="fa fa-facebook"></i></a>
-                                                        <a class="elementor-icon elementor-social-icon elementor-social-icon-twitter"
-                                                           href="https://twitter.com/zemezlab" target="_blank">
-                                                            <span class="elementor-screen-only">Twitter</span>
-                                                            <i class="fa fa-twitter"></i>
-                                                        </a>
-                                                        <a class="elementor-icon elementor-social-icon elementor-social-icon-google-plus"
-                                                           href="https://plus.google.com/" target="_blank">
-                                                            <span class="elementor-screen-only">Google-plus</span>
-                                                            <i class="fa fa-google-plus"></i>
-                                                        </a>
-                                                        <a class="elementor-icon elementor-social-icon elementor-social-icon-linkedin"
-                                                           href="https://www.linkedin.com/company/18088573/"
-                                                           target="_blank">
-                                                            <span class="elementor-screen-only">Linkedin</span>
-                                                            <i class="fa fa-linkedin"></i>
-                                                        </a>
-                                                        <a class="elementor-icon elementor-social-icon elementor-social-icon-pinterest"
-                                                           href="https://www.pinterest.com/" target="_blank">
-                                                            <span class="elementor-screen-only">Pinterest</span>
-                                                            <i class="fa fa-pinterest"></i>
-                                                        </a>
-                                                        <a class="elementor-icon elementor-social-icon elementor-social-icon-youtube"
-                                                           href="https://www.youtube.com/channel/UCPW43un8VFXHe9LxKpR_2Hg"
-                                                           target="_blank">
-                                                            <span class="elementor-screen-only">Youtube</span>
-                                                            <i class="fa fa-youtube"></i>
-                                                        </a>
+                                                <div class="">
+                                                    <div class="">
+                                                        <a class="" href="https://www.facebook.com/" target="_blank"><span class="">Facebook</span><i class="fab fa-facebook"></i></a>
+                                                        <a class="" href="https://instagram.com/" target="_blank"><span class="">Instagram</span><i class="fab fa-instagram"></i></a>
+                                                        <a class="" href="https://www.youtube.com/" target="_blank"><span class="">Youtube</span><i class="fab fa-youtube"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="elementor-element elementor-element-708abba elementor-align-right elementor-mobile-align-center elementor-widget__width-auto elementor-hidden-desktop elementor-hidden-phone elementor-widget-tablet__width-initial elementor-widget elementor-widget-button">
-                                                <div class="elementor-widget-container">
-                                                    <div class="elementor-button-wrapper">
-                                                        <a href="tel:8001231234" class="elementor-button-link elementor-button elementor-size-sm" role="button"><span class="elementor-button-content-wrapper"><span class="elementor-button-icon elementor-align-icon-left"><i class="fa fa-phone" aria-hidden="true"></i></span><span class="elementor-button-text">(800) 123 1234</span></span></a>
+                                            <div class="">
+                                                <div class="">
+                                                    <div class="">
+                                                        <a href="tel:8001231234" class="" role="button"><span class=""><span class=""><i class="fa fa-phone" aria-hidden="true"></i></span><span class="">(800) 123 1234</span></span></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -60,23 +35,17 @@
                             </div>
                         </div>
                     </section>
-                    <section
-                            class="elementor-element elementor-element-cc2ed06 elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
-                            data-id="cc2ed06" data-element_type="section"
-                            data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
-                        <div class="elementor-container elementor-column-gap-no">
-                            <div class="elementor-row">
-                                <div class="elementor-element elementor-element-4c7f7e8 elementor-column elementor-col-33 elementor-top-column"
-                                     data-id="4c7f7e8" data-element_type="column">
-                                    <div class="elementor-column-wrap  elementor-element-populated">
-                                        <div class="elementor-widget-wrap">
-                                            <div class="elementor-element elementor-element-d895680 elementor-widget elementor-widget-jet-logo"
-                                                 data-id="d895680" data-element_type="widget"
-                                                 data-widget_type="jet-logo.default">
-                                                <div class="elementor-widget-container">
-                                                    <div class="elementor-jet-logo jet-blocks">
-                                                        <div class="jet-logo jet-logo-type-image jet-logo-display-block">
-                                                            <div class="jet-logo__link"><img width="50" height="50" src="<?php echo get_template_directory_uri() . '/images/logo.jpg' ?>" class="jet-logo__img" alt="Speed" width="411" height="112"></div>
+                    <section class="">
+                        <div class="">
+                            <div class="">
+                                <div class="">
+                                    <div class="">
+                                        <div class="">
+                                            <div class="">
+                                                <div class="">
+                                                    <div class="">
+                                                        <div class="">
+                                                            <div class=""><img src="<?php echo get_template_directory_uri() . '/public/images/logo.jpg' ?>" class="" alt="Speed" width="411" height="112"></div>
                                                         </div>
                                                     </div>
                                                 </div>
