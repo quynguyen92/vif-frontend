701b1621303072f249c440b54e60bb1581492343
quynh <quynh@paraline.com.vn>
2019-07-05 14:21:33 +0700

    update service

---
+++ wp-content/themes/vif/app/views/services/index.php
@@ -0,0 +1,56 @@
+<section class="elementor-element-custom element-section">
+    <div class="element-container">
+        <div class="elementor-widget-wrap">
+            <section class="elementor-element-custom element-section">
+                <h2 class="elementor-heading-title elementor-size-default">OUR SERVICES</h2>
+            </section>
+            <section class="elementor-element-custom element-section">
+                <div class="element-container">
+                    <div class="elementor-row">
+                        <div class="elementor-col-50">
+                            <div class="elementor-column-wrap  elementor-element-populated">
+                                <div class="elementor-widget-wrap">
+                                    <div class="elementor-element-custom elementor-widget elementor-widget-icon-box">
+                                        <div class="elementor-widget-container-custom">
+                                            <div class="elementor-icon-box-wrapper">
+                                                <div class="elementor-icon-box-icon"><span class="elementor-icon elementor-animation-"><i class="fa fa-window-maximize" aria-hidden="true"></i></span></div>
+                                                <div class="elementor-icon-box-content">
+                                                    <h3 class="elementor-icon-box-title"><span>MAXIMUS DUI GRAV IDA</span></h3>
+                                                    <p class="elementor-icon-box-description">Etiam a erat
+                                                        rutrum, ornare augue nec placerat lectus. In
+                                                        efficitur dapibus quam, quis elementum est pharetra
+                                                        sit amet. Maecenas consequat libero augue</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="elementor-col-50">
+                            <div class="elementor-column-wrap elementor-element-populated">
+                                <div class="elementor-widget-wrap">
+                                    <div class="elementor-element-custom elementor-widget elementor-widget-icon-box">
+                                        <div class="elementor-widget-container-custom">
+                                            <div class="elementor-icon-box-wrapper">
+                                                <div class="elementor-icon-box-icon"><span class="elementor-icon elementor-animation-"><i class="fa fa-rocket" aria-hidden="true"></i></span></div>
+                                                <div class="elementor-icon-box-content">
+                                                    <h3 class="elementor-icon-box-title"><span>MAXIMUS DUI GRAV IDA</span></h3>
+                                                    <p class="elementor-icon-box-description">Vivamus at
+                                                        nulla sit amet orci ultrices cursus convallis sed
+                                                        sem. Maecenas dignissim tellus eu tincidunt
+                                                        auctor.Etiam a erat rutrum, ornare augue nec
+                                                        placerat</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</section>
