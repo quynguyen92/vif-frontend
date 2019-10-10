8901eb5a217a3e9425963b152dcb23bb2b5a4db1
quynh <quynh@paraline.com.vn>
2019-10-10 11:28:15 +0700

    fix bug not display chatbot

--- wp-content/themes/financepro/footer.php
+++ wp-content/themes/financepro/footer.php
@@ -50,5 +50,6 @@ switch ( $rdtheme_footer_column ) {
 </footer>
 </div>
 <?php wp_footer();?>
+<script async="" defer="" crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&amp;version=v4.0&amp;appId=1742293539329646&amp;autoLogAppEvents=1"></script>
 </body>
 </html>
