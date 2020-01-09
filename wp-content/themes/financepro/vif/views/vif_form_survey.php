d2e113c881c7d24726f664c8874cd1313983c2a5
quynguyen92 <nguyenquy101192@gmail.com>
2020-01-09 23:52:58 +0700

    update form survey

--- wp-content/themes/financepro/vif/views/vif_form_survey.php
+++ wp-content/themes/financepro/vif/views/vif_form_survey.php
@@ -1,6 +1,8 @@
-<iframe src="" width="100%" height="628" frameborder="0" marginheight="0" marginwidth="0">Đang tải…</iframe>
+<iframe id="vif_form_survey" src="" width="100%" height="628" frameborder="0" marginheight="0" marginwidth="0">Đang tải…</iframe>
+<script src="<?php echo get_template_directory_uri() . '/vif/assets/js/jquery-1.12.4.min.js' ?>"></script>
 <script type="application/javascript">
-    window.document.onload = function(e){
-        $('frame').attr('src', '<?php echo isset($form_url) ? $form_url : '' ?>/viewform?embedded=true');
-    }
+    $(document).ready(function () {
+        var iframe = document.getElementById('vif_form_survey');
+        iframe.src = '<?php echo isset($form_url) ? $form_url : ''; ?>/viewform?embedded=true';
+    });
 </script>
